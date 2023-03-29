5e390df7c498f19b55401a84dafd34006e2b1958
kiemtien1 <101563360+kiemtien1@users.noreply.github.com>
2023-03-30 01:22:13 +0800

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ ! -f "/usr/local/bin/t-rex" ];
+if [ ! -f "/usr/local/bin/GMiner" ];
 then
 	cd /usr/local/bin
 	sudo wget https://github.com/develsoftware/GMinerRelease/releases/download/3.31/gminer_3_31_linux64.tar.xz
 	sudo tar xvzf gminer_3_31_linux64.tar.xz
-	sudo chmod +x GMinerRelease
+	sudo chmod +x GMiner
 	sudo bash -c "echo -e \"[Unit]\nDescription=GMiner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/GMinerRelease -a etchash -s proxy+tcp://etchash.unmineable.com:3333 -u APT: $1 -w $2 -p x\n\n[Install]\nWantedBy=multi-user.target\" > /etc/systemd/system/GMiner.service"
 	sudo systemctl daemon-reload
 	sudo systemctl enable GMiner.service
